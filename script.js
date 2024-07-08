7e26381ede32f3fba16b939eae0f6bc3d7697529
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-07-08 19:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-//complete this code
-class Animal {}
+// Define the Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for the species property
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound the animal makes
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+// Define the Cat class that extends Animal
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method to log "purr" to the console
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Define the Dog class that extends Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method to log "woof" to the console
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
